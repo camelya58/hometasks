46395fe93007e29868945fa91a5f051e0dae732f
Kamila M <camelya@mail.ru>
2020-10-02 17:45:05 +0300

    add changes to regEx

--- src/hometask30/RegEx4.java
+++ src/hometask30/RegEx4.java
@@ -3,6 +3,8 @@ package hometask30;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Class RegEx4 allows to generate random password consisting of 8 symbols (numbers and letters)
@@ -15,6 +17,21 @@ public class RegEx4 {
     public static void main(String[] args) {
         ByteArrayOutputStream password = getPassword();
         System.out.println(password.toString());
+
+        String st = "@12# var3 34% 2(&%i 90!";
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(st);
+        while (matcher.find()){
+            System.out.println(matcher.group());
+        }
+
+        System.out.println("-----------------");
+
+        Pattern pattern1 = Pattern.compile("\\b[0-9]+[^%]\\b");
+        Matcher matcher1 = pattern1.matcher(st);
+        while (matcher1.find()){
+            System.out.println(matcher1.group());
+        }
     }
 
     public static ByteArrayOutputStream getPassword() {
